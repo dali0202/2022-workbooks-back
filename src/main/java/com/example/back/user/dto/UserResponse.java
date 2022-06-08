cfab216607e21f89759cd4c7a5e7c330fd97091c
dali0202 <keunhyung1996@gmail.com>
2022-06-08 10:20:20 +0900

    feat: 조건부 Route인 RequireAuth 도입 + userLoading State를 통한 새로고침시 로그인페이지로 이동하는 redirect 버그 수정

--- src/main/java/com/example/back/user/dto/UserResponse.java
+++ src/main/java/com/example/back/user/dto/UserResponse.java
@@ -10,16 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponse {
     private String name;
-
+    private String role;
     @Builder
-    public UserResponse(String name) {
+    public UserResponse(String name, String role) {
         this.name = name;
+        this.role = role;
     }
 
     public static UserResponse of(User user) {
         return UserResponse
                 .builder()
                 .name(user.getName())
+                .role(user.getRole().name())
                 .build();
     }
 }
