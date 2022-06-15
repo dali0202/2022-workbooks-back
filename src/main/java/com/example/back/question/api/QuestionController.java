fd1b450d79916c515c355e98fec31f32f8c820e7
dali0202 <keunhyung1996@gmail.com>
2022-06-16 05:28:37 +0900

    feat: cors설정

--- src/main/java/com/example/back/question/api/QuestionController.java
+++ src/main/java/com/example/back/question/api/QuestionController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/questions")
-@CrossOrigin(origins="*")
 public class QuestionController {
     private final QuestionService questionService;
 
